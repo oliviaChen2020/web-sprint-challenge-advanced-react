3598c0b0159c6df69b60f0c5388378b8a839f89b
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-10-30 13:11:01 -0400

    Checkout Form test built

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import CheckoutForm from './CheckoutForm';
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -8,6 +8,15 @@ test('form header renders', () => {
   render(<CheckoutForm />);
 
   const header = screen.getByText(/checkout form/i);
+
+  expect(header).toBeInTheDocument();
 });
 
-test('form shows success message on submit with form details', () => {});
+test('form shows success message on submit with form details', () => {
+  render(<CheckoutForm />);
+
+  const button = screen.getByRole('button');
+  fireEvent.click(button);
+
+  const formDetail = screen.findByText(/olivia/i);
+});
